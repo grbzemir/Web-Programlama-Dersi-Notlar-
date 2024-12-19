354d2ec291013093685c747d1fe3ae556be8ced1
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-12-19 20:27:48 +0300

    .

--- NotUygulamasi/script.js
+++ NotUygulamasi/script.js
@@ -3,9 +3,9 @@ const yeniNotBtn = document.getElementById("yeniNot");
 const notTut = JSON.parse(localStorage.getItem("notlar"));
 
 if (notTut) {
-    notTut.forEach((item) => {
-        ekleYeniNot(item);
-    });
+  notTut.forEach((item) => {
+    ekleYeniNot(item);
+  });
 }
 
 
@@ -29,47 +29,47 @@ function ekleYeniNot(text = "") {
     <div class="notSayfa ${text ? "" : "hidden"}"></div>
     <textarea class="${text ? "hidden" : ""}"></textarea>
     </div> `;
-   
-    const ekleBtn = not.querySelector(".ekle");
-    const silBtn = not.querySelector(".sil");
-    
-    const notSayfa = not.querySelector(".notSayfa");
-    const textArea = not.querySelector("textarea");
 
-    textArea.value = text;
-    notSayfa.innerHTML = marked(text);
-    
-    ekleBtn.addEventListener("click", () => {
-      notSayfa.classList.toggle("hidden");
-      textArea.classList.toggle("hidden");
-    });
-    
+  const ekleBtn = not.querySelector(".ekle");
+  const silBtn = not.querySelector(".sil");
 
-    silBtn.addEventListener("click", () =>{
-        not.remove();
-        guncelleLocalStorage();
+  const notSayfa = not.querySelector(".notSayfa");
+  const textArea = not.querySelector("textarea");
 
-    });
+  textArea.value = text;
+  notSayfa.innerHTML = marked(text);
 
-    textArea.addEventListener("input", (e) => {
-      const { value } = e.target;
-    
-      notSayfa.innerHTML = marked(value);
-      guncelleLocalStorage();
-    });
+  ekleBtn.addEventListener("click", () => {
+    notSayfa.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+
+
+  silBtn.addEventListener("click", () => {
+    not.remove();
+    guncelleLocalStorage();
+
+  });
+
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+
+    notSayfa.innerHTML = marked(value);
+    guncelleLocalStorage();
+  });
 
   document.body.appendChild(not);
 }
 
 function guncelleLocalStorage() {
-    const notYaz = document.querySelectorAll("textarea");
+  const notYaz = document.querySelectorAll("textarea");
 
-    const yazilanNot = [];
+  const yazilanNot = [];
 
-    notYaz.forEach((not) => {
-        yazilanNot.push(not.value);
-    });
+  notYaz.forEach((not) => {
+    yazilanNot.push(not.value);
+  });
 
-    localStorage.setItem("notlar", JSON.stringify(yazilanNot));
+  localStorage.setItem("notlar", JSON.stringify(yazilanNot));
 }
 
