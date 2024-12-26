c1b4539865a3e33d6428983cda840345b15cd423
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-12-26 22:43:36 +0300

    SliderProject

--- sliderprojesi/script.js
+++ sliderprojesi/script.js
@@ -4,34 +4,31 @@ const prevIcon = document.querySelector('.prev');
 const imageContainer = document.querySelector('.ImageContainer');
 const imgs = document.querySelectorAll('img');
 
-//gorselin sırasını tutalım
+//gorselin sırasını tutalım en başta olduğunu belirtmek için 1 dedik
 let currentImg = 1;
 
 let timeout;
 
-prevIcon.addEventListener('click', ()=> {
+prevIcon.addEventListener('click', () => {
     currentImg--;
     clearTimeout(timeout); // otomatik kaydirmayi sifilar
     updateImage();
 });
 
-nextIcon.addEventListener('click', ()=> {
+nextIcon.addEventListener('click', () => {
     currentImg++;
     clearTimeout(timeout); // otomatik kaydirmayi sifilar
     updateImage();
 });
 
-function updateImage()
-{
-    if(currentImg > imgs.length)
-    {
+function updateImage() {
+    if (currentImg > imgs.length) {
         currentImg = 1; // en basa döndür tekrardan başlat
     }
-    else if(currentImg < 1)
-    {
+    else if (currentImg < 1) {
         currentImg = imgs.length; // en sona kadar dönmeye devam eder.
     }
-    imageContainer.style.transform = `translateX(-${(currentImg-1)*700}px)`;
+    imageContainer.style.transform = `translateX(-${(currentImg - 1) * 700}px)`;
 
     timeout = setTimeout(() => {
         currentImg++;
